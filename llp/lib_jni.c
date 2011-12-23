0fbaf1a89057d5a01564e80de690a6ace8a2ea56
lvzixun <lvzixun@gmail.com>
2011-12-23 09:16:33 +0000

    对llp添加了jni的支持··

---
+++ llp/lib_jni.c
@@ -0,0 +1,256 @@
+#include "llp.h"
+#include "com_liteProto_LlpJavaNative.h"
+
+/*
+static char* jstringTostring(JNIEnv* env, jstring jstr)
+{
+	char* rtn = NULL;
+	jclass clsstring = (*env)->FindClass(env, "java/lang/String");
+	jstring strencode = (*env)->NewStringUTF(env, "utf-8");
+	jmethodID mid = (*env)->GetMethodID(env, clsstring, "getBytes", "(Ljava/lang/String;)[B");
+	jbyteArray barr= (jbyteArray)(*env)->CallObjectMethod(env, jstr, mid, strencode);
+	jsize alen = (*env)->GetArrayLength(env, barr);
+	jbyte* ba = (*env)->GetByteArrayElements(env, barr, JNI_FALSE);
+	if (alen > 0)
+	{
+		rtn = (char*)malloc(alen + 1);
+		memcpy(rtn, ba, alen);
+		rtn[alen] = 0;
+	}
+	(*env)->ReleaseByteArrayElements(env, barr, ba, 0);
+	return rtn;
+}
+
+static jstring stoJstring(JNIEnv* env, const char* pat)
+{
+	jclass strClass = (*env)->FindClass(env, "Ljava/lang/String;");
+	jmethodID ctorID = (*env)->GetMethodID(env, strClass, "<init>", "([BLjava/lang/String;)V");
+	jbyteArray bytes = (*env)->NewByteArray(env, strlen(pat));
+	jstring encoding = NULL;
+	(*env)->SetByteArrayRegion(env, bytes, 0, strlen(pat), (jbyte*)pat); 
+	encoding = (*env)->NewStringUTF(env, "utf-8");
+	return (jstring)(*env)->NewObject(env, strClass, ctorID, bytes, encoding);
+}
+*/
+
+static jbyteArray sliceTojbyteArray(JNIEnv* env, slice* sl)
+{
+	if(sl==NULL)
+		return (*env)->NewByteArray(env, 0);
+	else
+	{
+		jbyteArray jarray = (*env)->NewByteArray(env, (jsize)(sl->sp_size));
+		(*env)->SetByteArrayRegion(env, jarray, 0, (jsize)(sl->sp_size), (jbyte*)(sl->b_sp));
+		return jarray;
+	}
+}
+
+static jbyteArray stringTojbyteArray(JNIEnv* env, char* str)
+{
+	if(str==NULL)
+		return (*env)->NewByteArray(env, 0);
+	else
+	{
+		jsize lens = (jsize)(strlen(str));
+		jbyteArray jarray = (*env)->NewByteArray(env, lens);
+		(*env)->SetByteArrayRegion(env, jarray, 0, lens, (jbyte*)(str));
+		return jarray;
+	}
+}
+
+static void jbyteArrayToSlice(JNIEnv* env, jbyteArray jarray, slice* sl_out)
+{
+	if(sl_out==NULL)
+		return;
+	sl_out->b_sp = (*env)->GetByteArrayElements(env, jarray, 0);
+	sl_out->sp = sl_out->b_sp;
+	sl_out->sp_size =  (size_t)((*env)->GetArrayLength(env, jarray));
+}
+
+
+JNIEXPORT jlong JNICALL 
+Java_com_liteProto_LlpJavaNative_llpNewEnv(JNIEnv* jenv, jclass js)
+{
+	return (jlong)(llp_new_env());
+}
+
+JNIEXPORT void JNICALL 
+Java_com_liteProto_LlpJavaNative_llpFreeEnv(JNIEnv* jenv, jclass js, jlong env)
+{
+	llp_free_env((llp_env*)env);
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRegMes(JNIEnv* jenv, jclass js, jlong env, jbyteArray mesName)
+{
+	jint ret = 0;
+	char* mes_name = (char*)((*jenv)->GetByteArrayElements(jenv, mesName, JNI_FALSE));
+	ret = (jint)(llp_reg_mes((llp_env*)env, mes_name));
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpDelMes(JNIEnv* jenv, jclass js, jlong env, jbyteArray mesName)
+{
+	jint ret = 0;
+	char* mes_name = (char*)((*jenv)->GetByteArrayElements(jenv, mesName, JNI_FALSE));
+	ret = (jint)(llp_del_mes((llp_env*)env, mes_name));
+	return ret;
+}
+
+JNIEXPORT jlong JNICALL 
+Java_com_liteProto_LlpJavaNative_llpMessageNew(JNIEnv* jenv, jclass js, jlong env, jbyteArray mesName)
+{
+	jlong ret = 0;
+	char* mes_name = (char*)((*jenv)->GetByteArrayElements(jenv, mesName, JNI_FALSE));
+	ret = (jlong)(llp_message_new((llp_env*)env, mes_name));
+	return ret;
+}
+
+JNIEXPORT void JNICALL 
+Java_com_liteProto_LlpJavaNative_llpMessageClr(JNIEnv* jenv, jclass js, jlong inMes)
+{
+	llp_message_clr((llp_mes*)inMes);
+}
+
+JNIEXPORT void JNICALL 
+Java_com_liteProto_LlpJavaNative_llpMessageFree(JNIEnv* jenv, jclass js, jlong inMes)
+{
+	llp_message_free((llp_mes*)inMes);
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesInt32(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint number)
+{
+	jint ret = 0;
+	char* filede_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = llp_Wmes_int32((llp_mes*)lm, filede_str, (llp_int32)number);
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesInt64(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jlong number)
+{
+	jint ret = 0;
+	char* filede_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = llp_Wmes_int64((llp_mes*)lm, filede_str, (llp_int64)number);
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesFloat32(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jfloat number)
+{
+	jint ret = 0;
+	char* filede_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = llp_Wmes_float32((llp_mes*)lm, filede_str, (llp_float32)number);
+	return ret;
+}
+
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesFloat64(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jdouble number)
+{
+	jint ret = 0;
+	char* filede_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = llp_Wmes_float64((llp_mes*)lm, filede_str, (llp_float64)number);
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesString(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jbyteArray jstr)
+{
+	jint ret =0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	char* str = (char*)((*jenv)->GetByteArrayElements(jenv, jstr, JNI_FALSE));
+	ret = llp_Wmes_string((llp_mes*)lm, filed_str, str);
+	return ret;
+}
+
+JNIEXPORT jlong JNICALL 
+Java_com_liteProto_LlpJavaNative_llpWmesMessage(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr)
+{
+	jlong ret = 0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jlong)(llp_Wmes_message((llp_mes*)lm, filed_str));
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesInt32(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	jint ret = 0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jint)llp_Rmes_int32((llp_mes*)lm, filed_str, (unsigned int)alInx);
+	return ret;
+}
+
+JNIEXPORT jlong JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesInt64(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	jlong ret = 0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jlong)llp_Rmes_int64((llp_mes*)lm, filed_str, (unsigned int)alInx);
+	return ret;
+}
+
+JNIEXPORT jfloat JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesFloat32(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	jfloat ret = 0.0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jfloat)(llp_Rmes_float32((llp_mes*)lm, filed_str, (unsigned int)alInx));
+	return ret;
+}
+
+JNIEXPORT jdouble JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesFloat64(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	jdouble ret = 0.0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jdouble)llp_Rmes_float64((llp_mes*)lm, filed_str, (unsigned int)alInx);
+	return ret;
+}
+
+JNIEXPORT jbyteArray JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesString(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	char* rets = NULL;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	rets = llp_Rmes_string((llp_mes*)lm, filed_str, (unsigned int)alInx);
+	return stringTojbyteArray(jenv, rets);
+}
+
+JNIEXPORT jlong JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesMessage(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr, jint alInx)
+{
+	jlong ret = 0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jlong)(llp_Rmes_message((llp_mes*)lm, filed_str, (unsigned int)alInx));
+	return ret;
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpRmesSize(JNIEnv* jenv, jclass js, jlong lm, jbyteArray filedStr)
+{
+	jint ret = 0;
+	char* filed_str = (char*)((*jenv)->GetByteArrayElements(jenv, filedStr, JNI_FALSE));
+	ret = (jint)(llp_Rmes_size((llp_mes*)lm, filed_str));
+	return ret;
+}
+
+JNIEXPORT jbyteArray JNICALL 
+Java_com_liteProto_LlpJavaNative_llpOutMessage(JNIEnv* jenv, jclass js, jlong lm)
+{
+	slice* sret = NULL;
+	sret = llp_out_message((llp_mes*)lm);
+	return sliceTojbyteArray(jenv, sret);
+}
+
+JNIEXPORT jint JNICALL 
+Java_com_liteProto_LlpJavaNative_llpInMessage(JNIEnv* jenv, jclass js, jbyteArray jba, jlong lm)
+{
+	slice sl_in = {0};
+	jbyteArrayToSlice(jenv, jba, &sl_in);
+	return (jint)(llp_in_message(&sl_in, (llp_mes*)lm));
+}
+
