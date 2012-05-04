cc9b2c9195d8eac75aefa88bfe868ae0cc538b3c
lvzixun <lvzixun@gmail.com>
2012-05-04 07:09:08 +0000

    liteproto 对lua 的封装

---
+++ llplua.h
@@ -0,0 +1,36 @@
+#ifndef  _LLP_LUA_H_
+#define  _LLP_LUA_H_
+
+#include "llp.h"
+#include "lua.h"
+
+typedef enum _llpt{
+	LLPT_INT32,
+		LLPT_INT64,
+		LLPT_FLOAT32,
+		LLPT_FLOAT64,
+		LLPT_STRING,
+		LLPT_STREAM,
+		LLPT_MESSAGE
+} e_llpt; 
+
+typedef struct _ltype{
+	e_llpt t;
+
+	llp_uint8 is_repeated;
+	char* filed_name;
+}ltype;
+
+
+
+// ------- dump a message obj filed type
+// lms:		message obj
+// idx:		The current filed index at message obj (idx start at 1) 
+// lv_out:	the return value
+// return:	the next idx (the end is 0)
+llp_uint32 llp_Tnext(llp_mes* lms, llp_uint32 idx, ltype* lt_out);
+
+
+int llpL_open(lua_State* L, llp_env* env);
+
+#endif
