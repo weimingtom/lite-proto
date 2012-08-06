840fcb5a4ba87eacc60a82f59cf2157a65cf215e
lvzixun@gmail.com <lvzixun@gmail.com@9b7e1b84-1da1-73fe-bbac-3c5f79797841>
2012-08-06 14:51:40 +0000

    添加注释

--- branches/lite-proto2/llp/llplua.h
+++ branches/lite-proto2/llp/llplua.h
@@ -8,9 +8,9 @@
 
 // open env
 int llpL_open(lua_State* L, llp_env* env);
-// push a message obj
+// push a message obj with no gc function (the lm must is freed at c) 
 void lua_pushlm(lua_State* L, llp_mes* lm);
-// pop a message obj
+// pop a message obj (the lm not is freed at c, the lm has gc function at lua)
 llp_mes* lua_tolm(lua_State* L, int idx);
 
 #endif
