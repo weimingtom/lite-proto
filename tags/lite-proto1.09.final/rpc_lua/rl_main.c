83454934440f884f4f466a7aefafe3c10fe2e53f
lvzixun@gmail.com <lvzixun@gmail.com@9b7e1b84-1da1-73fe-bbac-3c5f79797841>
2012-08-06 14:59:40 +0000

    liteproto1.09 tag

---
+++ tags/lite-proto1.09.final/rpc_lua/rl_main.c
@@ -0,0 +1,54 @@
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+#include <windows.h>
+#include <stdio.h>
+#include "llp.h"
+#include "lrpcClib.h"
+#include "lrpcSlib.h"
+
+#pragma comment(lib, ".\\srpc_lua___Win32_Debug\\srpc_lua.lib")
+
+
+
+static int lua_t_time(lua_State* L)
+{
+	lua_pushnumber(L, (int)GetTickCount());
+	return 1;
+}
+
+
+slice* temp = NULL;
+int rpc_call_back(lua_State* L)
+{
+	temp = (slice*)lua_touserdata(L, -1);
+	return 0;
+}
+
+int rpc_error_cb(lua_State* L)
+{
+	char* error = (char*)lua_tostring(L, -1);
+	return 0;
+}
+
+int main()
+{
+	int ret = 0;
+	lua_State* L = lua_open();
+	luaopen_base(L);
+	luaopen_table(L);
+	luaopen_rpc(L, rpc_call_back, rpc_error_cb);
+
+	lua_register(L, "t_time", lua_t_time);
+	// client call
+	if(rpc_loadfile(L, "rpc_lua.lua"))
+	{
+		printf("[error]: %s", lua_tostring(L, -1));
+	}
+	
+	rpc_call_ret(L, rpc_call_func(L, temp));
+
+	lua_close(L);
+	mem_print();
+	return 0;
+}
